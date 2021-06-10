58c6b3cf7aec04bbd811b734648ec8900df77961
Fernando Perez <fernandoperezmerono@gmail.com>
2021-06-10 08:59:24 +0100

    next project with apollo server bascis

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import App from "next/app";
+import {ApolloProvider} from "@apollo/react-hooks";
+import withData from "../utils/apollo-client";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+class MyApp extends App {
+  render() {
+    const { Component, apollo } = this.props;
+
+    return (
+      <ApolloProvider client={apollo}>
+        <Component />
+      </ApolloProvider>
+    )
+  }
 }
 
-export default MyApp
+export default withData(MyApp);
+

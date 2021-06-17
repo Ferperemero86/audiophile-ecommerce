5fbf8576758bc74fbfe00a23d6af03ce3b6ae7f6
Fernando Perez <fernandoperezmerono@gmail.com>
2021-06-17 12:15:08 +0100

    test request

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,61 @@
-import {useQuery} from "@apollo/react-hooks";
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
-const USERS_QUERY = gql`
-  query customers {
-    getUser(id: 4) {
-      id
-      name
-      age
-      active
-    }
-  }
+const PRODUCTS_QUERY = gql`
+	query getProducts {
+		getProducts {
+			id
+			slug
+			image {
+				mobile
+				tablet
+				desktop
+			}
+			name
+			category
+			new
+			price
+			description
+			includes {
+				quantity
+				item
+			}
+			gallery {
+				first {
+					mobile
+					desktop
+					tablet
+				}
+				second {
+					mobile
+					desktop
+					tablet
+				}
+				third {
+					mobile
+					desktop
+					tablet
+				}
+			}
+			others {
+				slug
+				name
+				image {
+					mobile
+					tablet
+					desktop
+				}
+			}
+		}
+	}
 `;
 
 export default function Home() {
-  const {data, loading} = useQuery(USERS_QUERY);
+	const { data, loading } = useQuery(PRODUCTS_QUERY);
 
-  if (!loading) {
-    console.log("DATA", data);
-  }
-  return <h1>Home</h1>
+	if (!loading) {
+		console.log("DATA", data);
+	}
+	return <h1>Home</h1>;
 }

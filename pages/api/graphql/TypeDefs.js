fe50ba79bda9f0039f6f5dab25868b539e42bc26
Fernando Perez <fernandoperezmerono@gmail.com>
2021-06-17 11:22:25 +0100

    Create schema

--- pages/api/graphql/TypeDefs.js
+++ pages/api/graphql/TypeDefs.js
@@ -1,17 +1,48 @@
-import {gql} from "apollo-server-micro";
+import { gql } from "apollo-server-micro";
 
-const typeDefs = gql `
-    type User {
-        id: Int!
-        name: String!
-        age: Int
-        active: Boolean!
-    }
+const typeDefs = gql`
+	type Image {
+		mobile: String
+		tablet: String
+		desktop: String
+	}
 
-    type Query {
-        getUser(id: Int): User
-        getUsers: [User]
-    }
+	type Accesory {
+		quantity: Int
+		item: String
+	}
+
+	type Gallery {
+		first: Image
+		second: Image
+		third: Image
+	}
+
+	type RelatedProduct {
+		slug: String
+		name: String
+		image: Image
+	}
+
+	type Product {
+		id: Int
+		slug: String
+		image: Image
+		name: String
+		category: String
+		new: Boolean
+		price: Int
+		description: String
+		features: String
+		includes: [Accesory]
+		gallery: Gallery
+		others: [RelatedProduct]
+	}
+
+	type Query {
+		getProduct(id: Int): Product
+		getProducts: [Product]
+	}
 `;
 
 export default typeDefs;

6c272bb3255d6743391f8f0b7c64017615a58659
Fernando Perez <fernandoperezmerono@gmail.com>
2021-06-26 11:19:11 +0100

    Add section one and three in models page

--- pages/models.js
+++ pages/models.js
@@ -5,6 +5,10 @@ import { useQuery, gql } from "@apollo/client";
 
 import Header from "../components/layout/header/Header";
 import SectionFour from "../components/layout/sections/section-four/SectionFour";
+import SectionOne from "../components/layout/sections/section-one/SectionOne";
+import SectionThree from "../components/layout/sections/section-three/SectionThree";
+
+import { appData } from "../appData";
 
 const QUERY = gql`
 	query getModels($category: String) {
@@ -25,6 +29,7 @@ const Models = () => {
 	const router = useRouter();
 	const { query } = router;
 	const headerTitle = query.category.toUpperCase();
+	const casualContent = appData.casual;
 
 	const { data, loading } = useQuery(QUERY, {
 		variables: {
@@ -34,12 +39,16 @@ const Models = () => {
 
 	if (!loading) {
 		const modelsItems = data.getProducts;
-		console.log("DATA", data);
 
 		return (
 			<div className="models">
 				<Header type="header-second" title={headerTitle} />
 				<SectionFour modelsItems={modelsItems} />
+				<SectionOne stylesClass="container vertical-margin" />
+				<SectionThree
+					stylesClass="container vertical-margin"
+					content={casualContent}
+				/>
 			</div>
 		);
 	}

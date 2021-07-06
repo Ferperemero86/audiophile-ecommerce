9e3e8177ec3394e33f63140a13b1e5fe5c5f30a4
Fernando Perez <fernandoperezmerono@gmail.com>
2021-07-06 05:33:42 +0100

    Add fixed background when displaying cart

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -8,9 +8,10 @@ const Layout = ({ children }) => {
 	const state = useSelector((state) => state);
 	const cartDisplay = state.cart.display;
 	const blurBackground = cartDisplay ? "blur" : "";
+	const staticBackground = cartDisplay ? "fixed o-flow-hidden" : "";
 
 	return (
-		<div className="layout wrapper">
+		<div className={`layout wrapper ${staticBackground}`}>
 			<MainMenu />
 			<div className={`layout-content ${blurBackground}`}>{children}</div>
 			<Footer />

0c871ecb7045695113a00a118994e672ce78129f
Fernando Perez <fernandoperezmerono@gmail.com>
2021-07-15 11:01:25 +0100

    Remove button from header

--- components/ui/product/intro/ProductIntro.js
+++ components/ui/product/intro/ProductIntro.js
@@ -1,15 +1,12 @@
 import React from "react";
 
-import Button from "../../button/Button";
-
-const ProductIntro = ({ product, button }) => {
+const ProductIntro = ({ product }) => {
 	const { heading, text } = product;
 
 	return (
 		<div className="product-intro">
 			<h1 className="product-intro-heading">{heading}</h1>
 			<p className="product-intro-text">{text}</p>
-			<Button stylesClass="product-intro-button" label={button.label} />
 		</div>
 	);
 };
